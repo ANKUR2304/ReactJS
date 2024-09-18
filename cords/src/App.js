a3c6f2fb8a0a2dd2b6ab90505335d0c7daf965c7
ANKUR2304 <jakharankur5@gmail.com>
2024-09-19 01:13:22 +0530

    Initial project setup for Cords

---
+++ cords/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import MouseCords from "./mouseCords";
+
+function App() {
+  return (
+    <div className="App">
+      <MouseCords />
+    </div>
+  );
+}
+
+export default App;
---
+++ cords/src/mouseCords.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MouseCords() {
+  return (
+    <div>
+      <h1>ReactJS Component</h1>
+    </div>
+  );
+}
+
+export default MouseCords;
