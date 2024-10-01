9e60cbaac047772e8bb22bb546d72b5222af63a5
ANKUR2304 <jakharankur5@gmail.com>
2024-10-02 02:21:21 +0530

    Adding Redux

---
+++ Ecart/src/App.js
@@ -0,0 +1,18 @@
+import Purchase from './components/Purchase';
+import Cart from './components/Cart';
+import Total from './components/Total';
+import Login from './components/Login';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Login/>
+      <Purchase/>
+      <Cart/>
+      <Total/>
+    </div>
+  );
+}
+
+export default App;
